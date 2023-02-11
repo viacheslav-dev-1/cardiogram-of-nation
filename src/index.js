4fbd365630361a6690d07242c7759649b2629bb7
viacheslav <bviacheslav.12@gmail.com>
2023-02-11 12:26:09 +0200

    fixed styles unexpected behavior

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import { Db } from './js/db';
 import { ChartDrawings } from './js/chart/chartDrawings';
 import { Store } from './js/store/Store'
 
-(function start() {
+const imports = Init.isMobile ? loadMobileStyles() : loadDesktopStyles()
+imports.then(start)
 
+function start() {
+    document.getElementById('container').style.display = 'flex'
+    
     Init.store()
 
     /* Load styles */
@@ -36,38 +40,35 @@ import { Store } from './js/store/Store'
     const drawings = new ChartDrawings('chart', warDay, 7)
 
     /* Draw chart */
-    setTimeout(() => {
-        const lastUpdateLs = localStorage.getItem('lastUpdate')
-        if (lastUpdateLs === undefined || lastUpdateLs === null || lastUpdateLs !== Init.date(Date.now())) {
-            db.get().then(data => {
-                const sorted = data.sort((prev, next) => parseInt(prev.day) > parseInt(next.day) ? 1 : -1)
-                document.getElementById('loader').style.display = 'none'
-                drawings.draw(sorted, { darker: 'black', dark: '#3e3e3e', bright: '#c9c9c9', blue: '#53b1f9', yellow: '#fffb00', red: '#ff5f5f', zero: 'white', zeroStroke: '#ff5f5f' })
-                localStorage.setItem('lastUpdate', Init.date(Date.now()))
-                localStorage.setItem('emotionsData', JSON.stringify(sorted))
-            })
-        } else {
+    const lastUpdateLs = localStorage.getItem('lastUpdate')
+    if (lastUpdateLs === undefined || lastUpdateLs === null || lastUpdateLs !== Init.date(Date.now())) {
+        db.get().then(data => {
+            const sorted = data.sort((prev, next) => parseInt(prev.day) > parseInt(next.day) ? 1 : -1)
             document.getElementById('loader').style.display = 'none'
+            drawings.draw(sorted, { darker: 'black', dark: '#3e3e3e', bright: '#c9c9c9', blue: '#53b1f9', yellow: '#fffb00', red: '#ff5f5f', zero: 'white', zeroStroke: '#ff5f5f' })
+            localStorage.setItem('lastUpdate', Init.date(Date.now()))
+            localStorage.setItem('emotionsData', JSON.stringify(sorted))
+        })
+    } else {
+        document.getElementById('loader').style.display = 'none'
 
-            const dataLs = localStorage.getItem('emotionsData')
-            drawings.draw(JSON.parse(dataLs), { darker: 'black', dark: '#3e3e3e', bright: '#c9c9c9', blue: '#53b1f9', yellow: '#fffb00', red: '#ff5f5f', zero: 'white', zeroStroke: '#ff5f5f' })
-        }
-    }, 200)
-
-})();
+        const dataLs = localStorage.getItem('emotionsData')
+        drawings.draw(JSON.parse(dataLs), { darker: 'black', dark: '#3e3e3e', bright: '#c9c9c9', blue: '#53b1f9', yellow: '#fffb00', red: '#ff5f5f', zero: 'white', zeroStroke: '#ff5f5f' })
+    }
+}
 
 function loadDesktopStyles() {
-    import('./styles/modal.scss').then()
-    import('./styles/chart.scss').then()
-    import('./styles/header-desktop.scss').then()
-    
     document.getElementsByClassName('top-action-panel')[0].style.display = 'none'
+    
+    return import('./styles/modal.scss')
+        .then(() => import('./styles/chart.scss')
+                        .then(() => import('./styles/header-desktop.scss')))
 }
 
 function loadMobileStyles() {
-    import('./styles/modal-mobile.scss').then()
-    import('./styles/chart.scss').then()
-    import('./styles/header-mobile.scss').then()
-
     document.getElementsByClassName('bottom-action-panel')[0].style.display = 'none'
+
+    return import('./styles/modal-mobile.scss')
+        .then(() => import('./styles/chart.scss')
+                        .then(() => import('./styles/header-mobile.scss')))
 }
