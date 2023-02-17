71c910b3aee945b44ab7f00b8748e136b510a600
viacheslav <bviacheslav.12@gmail.com>
2023-02-17 11:17:05 +0200

    Removed clicking on menu item

--- src/components/settings.js
+++ src/components/settings.js
@@ -13,10 +13,7 @@ export class SettingsComponent {
 
         const asTaras = document.getElementById('asTaras')
         data && data.isMobile && asTaras.remove()
-
-        const content = document.getElementById('settingsContent')
-        let howWorksB = true
-        let aboutB = true
+        
         let asTarasB = localStorage.getItem('asTaras') == 1 ? true : false
         
         document.getElementById('howWorksContent').innerHTML = howWorks
@@ -33,19 +30,6 @@ export class SettingsComponent {
                 localStorage.setItem('asTaras', asTarasB ? 1 : 0)
             })
         }
-
-        content.addEventListener('click', e => {
-            if (e && e.target) {
-                if (e.target.closest('#howWorks')) {
-                    document.getElementById('howWorksContent').innerHTML = howWorksB ? '' : howWorks
-                    howWorksB = !howWorksB
-                }
-                else if (e.target.closest('#about')) {
-                    document.getElementById('aboutContent').innerHTML = aboutB ? '' : about
-                    aboutB = !aboutB
-                }
-            }
-        })
 
         closeBtn.addEventListener('click', () => {
             aboutMask.style.display = 'none'
