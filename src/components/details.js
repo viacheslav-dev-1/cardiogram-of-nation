7f579b276ef94c4c8a7becf3757920eb8288a693
viacheslav <bviacheslav.12@gmail.com>
2023-03-28 11:01:37 +0300

    Added zero-points to details and full date

--- src/components/details.js
+++ src/components/details.js
@@ -13,13 +13,21 @@ export class DetailsComponent {
         })
 
         const detailsDay = document.getElementById('detailsDay')
-        detailsDay.innerHTML = `<a>${data} день війни</a>`
+        const result = new Date('02/24/2022')
+        result.setDate(result.getDate() + data - 1)
+        const dayV = result.getUTCDate()
+        let monthV = result.getUTCMonth()
+        monthV = monthV < 10 ? '0' + monthV : monthV
+        const yearV = result.getUTCFullYear();
+        detailsDay.innerHTML = `<a>${data} день війни</a><br/><small>${dayV}.${monthV}.${yearV}</small>`
 
         const dayData = Store.get('eventData').cur.filter(it => it.day === data)[0]
         document.getElementById('cFeel').innerText = dayData?.cFeel ? dayData.cFeel : "-"
         document.getElementById('sFeel').innerText = dayData?.sFeel ? dayData.sFeel : "-"
         document.getElementById('eFeel').innerText = dayData?.eFeel ? dayData.eFeel : "-"
         document.getElementById('trigger').innerText = dayData?.trigger ? dayData.trigger : "-"
+        document.getElementById('cZero').style.display = dayData?.cZero === true ? '' : 'none'
+        document.getElementById('sZero').style.display = dayData?.sZero === true ? '' : 'none'
 
         const day = data < 10 ? '0'+data : data
         document.getElementById('video')
